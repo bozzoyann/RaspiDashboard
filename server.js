c4fbf14046f7ca268483694ff94b8baf53990cd8
bozzoyann <yann.longere@gmail.com>
2016-10-16 09:26:31 +0200

    add very basic nodejs server
    base of a nodejs server

---
+++ server.js
@@ -0,0 +1,23 @@
+console.log("coucou le monde, je suis node.js");
+
+var http=require('http');
+var port=8080;
+
+console.log('NodeJS server started listening'+port);
+
+var server= http.createServer(function(req,res){
+	res.writeHead(200,{"content-type":"text/html"});
+	res.write('<!DOCTYPE html>'+
+'<html>'+
+'    <head>'+
+'        <meta charset="utf-8" />'+
+'        <title>Ma page Node.js !</title>'+
+'    </head>'+ 
+'    <body>'+
+'     	<p>Voici un paragraphe <strong>HTML</strong> !</p>'+
+'    </body>'+
+'</html>');
+	res.end("<h1>Moi aussi</h1>");
+});
+
+server.listen(port);
