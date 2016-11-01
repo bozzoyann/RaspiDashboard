415fa00cd9f1cc020ee5e0034ac6cfb361d0ecee
thomas <thomasr2309@gmail.com>
2016-11-01 15:06:23 +0100

    add build configuration

---
+++ GulpFile.js
@@ -0,0 +1,39 @@
+var gulp = require("gulp");
+var merge = require("merge2");
+var clean = require("gulp-clean");
+var browserify = require("gulp-browserify");
+
+gulp.task("clean", function() {
+    return createCleanStream();
+});
+
+gulp.task("install", function() {
+    var installServerStream = createInstallServerStream();
+    var installCliStream = createInstallCliStream();
+    var installServicesStream = createInstallServicesStream();
+    return merge(installServerStream, installCliStream, installServicesStream);
+});
+
+function createInstallServerStream() {
+    return gulp.src("src/main/server/**/*.js")
+    .pipe(gulp.dest("target/server"));
+}
+
+function createInstallCliStream() {
+    return gulp.src("src/main/client/**")
+    .pipe(gulp.dest("target/server/public"));
+}
+
+function createInstallServicesStream() {
+    return gulp.src("src/main/services/**")
+    .pipe(gulp.dest("target/services"));
+}
+
+function createCleanStream() {
+    return gulp.src("target", {
+        read: false
+    })
+    .pipe(clean({
+        force: true
+    }));
+}
--- src/main/server/main.js
+++ src/main/server/main.js
@@ -3,7 +3,7 @@ var path = require("path");
 var express = require("express");
 var app = express();
 
-app.use(express.static(path.join(__dirname, "../client")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/api/test", function(request, response) {
     console.log("Receive a request");
