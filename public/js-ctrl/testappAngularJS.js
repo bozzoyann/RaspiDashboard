24db2ca18f2a423218fb5bed28df3f1e76c646e4
bozzoyann <yann.longere@gmail.com>
2016-11-02 22:42:28 +0100

    Revert "Test de angular"
    This reverts commit de52b4b2005aab97128452b1c3fb56b2f9fa667b.

--- public/js-ctrl/testappAngularJS.js
+++ public/js-ctrl/testappAngularJS.js
@@ -1,9 +1,8 @@
 'use stict'
 
-var myAppTestAngular = angular.module('myAppTestAngular', []);
+var apptest = angular.module('apptest', []);
 
-myAppTestAngular.controller('ctrltest1', ['$scope', '$interval' , function ($scope, $interval) {
-
+apptest.controller('ctrltest1', ['$scope', '$interval' , function ($scope, $interval) {
     var time_format = 'M/d/yy h:mm:ss';
 
 	function heure(){
@@ -13,15 +12,15 @@ myAppTestAngular.controller('ctrltest1', ['$scope', '$interval' , function ($sco
 		var minute = date.getMinutes();
 		var seconde = date.getSeconds();
 		var texte = heure+" : "+minute+" : "+seconde;
+		//return(heure+" : "+minute+" : "+seconde);
+		//$scope.horloge = texte;
 		$scope.horloge = texte;
+		//return texte;
 
 	};
 
-	//heure();
-
-	//$interval(heure(), 1000);
-
-	var interval = setInterval(heure(), 1000);
+	//$scope.horloge  =heure();
+	$interval(heure(), 1000);
 
 	$scope.message='coucou';
 
@@ -42,20 +41,3 @@ myAppTestAngular.controller('ctrltest1', ['$scope', '$interval' , function ($sco
 	
 
 }]);
-
-
-//Pourquoi ça marche ce truc !!!?!!?
-angular.module('myAppTestAngular').directive('horloge', function () {
-	return {
-		link: function (scope, element, attrs) {
-			function afficheHeure(){
-				var date = new Date();
-				var heure = date.getHours();
-				var minute = date.getMinutes();
-				var seconde = date.getSeconds();
-				element.text(heure+" : "+minute+" : "+seconde);
-			}
-			var interval = setInterval(afficheHeure,1000);
-		}
-	};
-});
